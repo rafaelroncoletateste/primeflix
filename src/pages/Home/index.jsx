6e7c1e947913f2ad41ade624ea95f4389e82a5ae
Rafael <rafaelnh089@hotmail.com>
2026-03-06 19:29:53 -0300

    Criando nossa Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+
+import api from "../../services/api";
+
 export default function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  const chaveAPI = import.meta.env.VITE_API_KEY;
+
+  useEffect(() => {
+    async function loadMovies() {
+      await api.get("movie/now_playing", {
+        params: {
+          api_key: chaveAPI,
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+    }
+
+    loadMovies();
+  }, []);
+
   return (
     <div>
       <h1>Página Home</h1>
