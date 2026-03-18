d0a5c17098f204c822877fe87e7838f5e37d7867
Rafael <rafaelnh089@hotmail.com>
2026-03-18 18:48:42 -0300

    Detalhes do filme

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,7 +1,54 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from "../../services/api";
+
 export default function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const chaveAPI = import.meta.env.VITE_API_KEY;
+
+  useEffect(() => {
+    async function loadMovie() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: chaveAPI,
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch((err) => console.log(err));
+    }
+
+    loadMovie();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Acessando Filme!</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Página Filme</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+
+      <h3>Sinopse</h3>
+      <p>{filme.overview}</p>
+
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
     </div>
   );
 }
